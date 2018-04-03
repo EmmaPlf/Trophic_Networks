9efb3e4d489699366bdd362ec23450d1b93e1495
EmmaPlf <emma.palfi@edu.ece.fr>
2018-04-03 23:28:11 +0200

    Chargement des fichiers

---
+++ main.cpp
@@ -0,0 +1,63 @@
+#include "grman/grman.h"
+#include <iostream>
+
+#include "graph.h"
+
+//BITMAP* charger = load_bitmap("Charger.bmp", NULL);
+
+int main()
+{
+    bool asterix = false, livre = false;
+
+    /// A appeler en 1er avant d'instancier des objets graphiques etc...
+    grman::init();
+
+    /// Le nom du répertoire où se trouvent les images à charger
+    grman::set_pictures_path("pics");
+
+    /// CHARGEMENT DU GRAPHE
+    Graph g;
+    g.make_example();
+
+    Graph g2;
+    g2.make_example();
+
+    /// Vous gardez la main sur la "boucle de jeu"
+    /// ( contrairement à des frameworks plus avancés )
+    while ( !key[KEY_ESC] )
+    {
+        /// Il faut appeler les méthodes d'update des objets qui comportent des widgets
+        g.update();
+
+     //grman::show_picture(grman::page,"Asterix.bmp", 23,5,0);
+
+     g.getInterface()->getAsterix().interact_focus();
+
+     if(g.getInterface()->getAsterix().clicked()==true &&  asterix == false )
+        {
+             g.chargement_fichier("Asterix.txt");
+                asterix = true;
+        }
+
+     //grman::show_picture(grman::page,"Asterix.bmp", 23,5,0);
+
+     g2.getInterface()->getLivre().interact_focus();
+
+     if(g2.getInterface()->getLivre().clicked()==true && livre == false)
+        {
+             g2.chargement_fichier("Livre_jungle.txt");
+             livre = true;
+        }
+
+
+        /// Mise à jour générale (clavier/souris/buffer etc...)
+        grman::mettre_a_jour();
+    }
+
+    grman::fermer_allegro();
+
+    return 0;
+}
+END_OF_MAIN();
+
+
